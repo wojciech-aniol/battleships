08f43eb9bec0967dd8994ecf47368dba87a07084
wojtek474 <wojciechaniol474@gmail.com>
2017-06-11 19:03:03 +0200

    second board, second player

--- board.py
+++ board.py
@@ -14,26 +14,28 @@ class Board(object):
         self.width = cols
         self.height = rows
         self.grid = []
-        self.computer_grid = []
         # nodes = [[Node() for j in range(cols)] for i in range(rows)]
         self.grid = [ [Field() for j in range(cols)] for i in range(rows) ]
-        self.computer_grid = [ [Field() for j in range(cols)] for i in range(rows) ]
+        self.total_ships_length = 0
+        self.hit_shots = 0
+        self.last_shot = False
+        self.last_shot_row = 0
+        self.last_shot_col = 0
+
+        #New ship values
+        self.new_ship_col = []
+        self.new_ship_row = []
+        self.new_ship_length = 0
+        self.new_ship_orientation = Board.HORIZONTAL
 
     def is_game_finished(self):
         flat = sum(self.grid, [])
         revealed_field_tester = lambda field: field.ship and (not field.reveal)
         return len(filter(revealed_field_tester, flat)) == 0
 
-    def are_fields_empty(self, length, orientation, row, col):
-        if orientation == Board.HORIZONTAL and col+length<=BOARDWIDTH:
-            for current_column in range(col+length):
-                if self.grid[row][current_column].ship == True:
-                    return False
-        elif orientation == Board.VERTICAL and row+length<=BOARDHEIGHT:
-            for current_row in range(row, row+length):
-                if self.grid[current_row][col].ship == True:
-                    return False
-        return True
+    def add_random_ships(self, num):
+        for i in range(1, num+1):
+            self.add_random_ship(i)
 
     def add_random_ship(self, length):
         orientation = [Board.HORIZONTAL, Board.VERTICAL]
@@ -42,15 +44,9 @@ class Board(object):
         direction = random.choice(orientation)
         try:
             self.add_ship(length=length, orientation=random.choice(orientation), row=row, col=col)
-        except exceptions.FieldTakenException as e:
+            self.total_ships_length += length
+        except:
             self.add_random_ship(length)
-
-    def add_random_ships(self, num):
-        total_length = 0
-        for i in range(1, num+1):
-            self.add_random_ship(i)
-            total_length += i
-        return total_length
 
     def add_ship(self, length, orientation, row, col):
         # FIXME! sprawdz czy nie istnieje tu inny statek albo jestes poza plansza FIXME!
@@ -67,6 +63,84 @@ class Board(object):
         else:
              raise FieldTakenException("Ship already exists on this place")
 
+    def add_ships_manual(self, number_of_human_ships, row, col):
+        # orientation = [Board.HORIZONTAL, Board.VERTICAL]
+        # direction = random.choice(orientation)
+        # self.add_ship(number_of_human_ships, direction, row, col)
+        print "Wspolrzedne [{}][{}], statek dlugosci {}/{}".format(col, row, self.new_ship_length , number_of_human_ships)
+        if self.new_ship_length == number_of_human_ships:
+            self.add_ship(self.new_ship_length, self.new_ship_orientation, self.new_ship_row[0], self.new_ship_col[0])
+            print "new_ship_length {} == {} number_of_human_ships".format(self.new_ship_length, number_of_human_ships)
+
+            #Reset values
+            print "Resetuje wartosci statku {}".format(number_of_human_ships)
+            self.new_ship_col = []
+            self.new_ship_row = []
+            self.new_ship_length = 0
+            self.new_ship_orientation = Board.HORIZONTAL
+
+            print "Zwracam True dla statku numer {}".format(number_of_human_ships)
+            return True
+        else:
+            if self.new_ship_length == 0:
+                self.new_ship_col.append(col)
+                self.new_ship_row.append(row)
+                self.new_ship_length += 1
+                print "nowa dlugosc statku: {}".format(self.new_ship_length)
+                self.add_ships_manual(number_of_human_ships, row, col)
+                return False
+            elif self.new_ship_length == 1:
+                print "new_ship_length = 1"
+                if col != self.new_ship_col[0] and row != self.new_ship_row[0]:
+                    print "Pierwszy if"
+                    return False
+                elif col != self.new_ship_col[0] and row == self.new_ship_row[0]:
+                    print "Drugi if"
+                    self.new_ship_orientation = Board.VERTICAL
+                    self.new_ship_col.append(col)
+                    self.new_ship_row.append(row)
+                    self.new_ship_length += 1
+                    self.add_ships_manual(number_of_human_ships, row, col)
+                    return False
+                elif col == self.new_ship_col[0] and row != self.new_ship_row[0]:
+                    print "Trzeci if"
+                    self.new_ship_orientation = Board.HORIZONTAL
+                    self.new_ship_col.append(col)
+                    self.new_ship_row.append(row)
+                    self.new_ship_length += 1
+                    self.add_ships_manual(number_of_human_ships, row, col)
+                    return False
+            elif self.new_ship_length >= 2:
+                if self.orientation == Board.VERTICAL:
+                    if col != self.new_ship_col[0]:
+                        return False
+                    else:
+                        self.new_ship_col.append(col)
+                        self.new_ship_row.append(row)
+                        self.new_ship_length += 1
+                        self.add_ships_manual(number_of_human_ships, row, col)
+                        return False
+                elif self.orientation == Board.HORIZONTAL:
+                    if row != self.new_ship_row[0]:
+                        return False
+                    else:
+                        self.new_ship_col.append(col)
+                        self.new_ship_row.append(row)
+                        self.new_ship_length += 1
+                        self.add_ships_manual(number_of_human_ships, row, col)
+                        return False
+
+    def are_fields_empty(self, length, orientation, row, col):
+        if orientation == Board.HORIZONTAL and col+length<=BOARDWIDTH:
+            for current_column in range(col+length):
+                if self.grid[row][current_column].ship or self.grid[row+1][current_column].ship or self.grid[row-1][current_column].ship:
+                    return False
+        elif orientation == Board.VERTICAL and row+length<=BOARDHEIGHT:
+            for current_row in range(row, row+length):
+                if self.grid[current_row][col].ship or self.grid[current_row][col-1].ship or self.grid[current_row][col+1].ship:
+                    return False
+        return True
+
     def get_color(self, row, col):
         field = self.grid[row][col]
         if field.ship == True:
@@ -77,21 +151,42 @@ class Board(object):
         return green
 
     def click_ship(self, row, col):
-        # ship = self.grid[row][col]
-        # is_ship_present = ship in not True
-        # if is_ship_present:
-        #     # print "setting reveal at {} {}".format(row, col)
-        #     ship.reveal = True
-        # return is_ship_present
-        #-------
         field = self.grid[row][col]
-        if not field.reveal:
-            field.reveal = True
-            if field.ship:
-                return True
-        if field.ship and field.reveal:
+        if field.reveal:
             return False
+        else:
+            field.reveal = True
+            # self.last_shot = False
+            if field.ship:
+                self.hit_shots += 1
+                self.last_shot = True
+                self.last_shot_col = col
+                self.last_shot_row = row
+            return True
 
+    def near_shot(self):
+        row = random.randint(self.last_shot_row-1,self.last_shot_row+1)
+        col = random.randint(self.last_shot_col-1, self.last_shot_col+1)
+        if row >= BOARDWIDTH:
+            row = BOARDWIDTH-1
+        if col >= BOARDHEIGHT:
+            col = BOARDWIDTH
+        if row != self.last_shot_row:
+            col=self.last_shot_col
+        else:
+            row = self.last_shot_row
+        try:
+            if not self.click_ship(row, col):
+                self.near_shot()
+        except RuntimeError as re:
+            if re.args[0] != 'maximum recursion depth exceeded while calling a Python object':
+                # different type of runtime error
+                raise
+            self.last_shot = False
+            self.random_shot()
 
-        # if field.ship == True:
-            # field.reveal = True
+    def random_shot(self):
+        row = random.randint(0,BOARDHEIGHT-1)
+        col = random.randint(0, BOARDWIDTH-1)
+        if not self.click_ship(row, col):
+            self.random_shot()
--- game.py
+++ game.py
@@ -7,20 +7,29 @@ from board import Board
 
 class Game(object):
 
+    HUMAN = True
+    COMPUTER = False
+
     def __init__(self):
         #Initialization
-        self.total_shots = 0
         self.hit_shots = 0
+        self.player = self.HUMAN
+        self.number_of_ships = 2
+
+        self.is_human_ships_added = False
+        self.number_of_human_ships = 1
+
 
         pygame.init()
         self.myfont = pygame.font.SysFont("monospace", 30)
 
         self.board = Board(BOARDHEIGHT, BOARDWIDTH)
+        self.comp_board = Board(BOARDHEIGHT, BOARDWIDTH)
         # self.board.add_ship(length=3, orientation=Board.HORIZONTAL, row=0, col=0)
         self.screen = pygame.display.set_mode((window_width, window_height))
-        self.total_shots = self.board.add_random_ships(2)
-
-        # self.debug()
+        self.board.add_random_ships(self.number_of_ships) #Add computer ship on human board
+        # self.comp_board.add_random_ships(5) #Add player ship on computer board
+        # self.board.add_ships_manual(5)
 
         while True:
             #Handle events
@@ -30,17 +39,34 @@ class Game(object):
                     sys.exit()
                 elif event.type == MOUSEBUTTONUP: #otherwise
                     mousex, mousey = event.pos #set mouse positions to the new position
-                    #mouse_clicked = True #mouse is clicked but not on a button
-                    col, row = self._get_tile_at_pixel(mousex, mousey)
-                    if isinstance(col, int):
-                        if self.board.click_ship(row, col):
-                            self.hit_shots += 1
-                    if self.board.is_game_finished():
-                        self.end_game()
-                        break
-
-            if not self.board.is_game_finished():
+                    col, row = self._get_tile_at_pixel(self.is_human_ships_added, mousex, mousey)
+                    if self.player and self.is_human_ships_added:
+                        if isinstance(col, int):
+                            if self.board.click_ship(row, col):
+                                self.player = Game.COMPUTER
+                                self.computer_turn()
+                        if self.board.is_game_finished():
+                            self.end_game()
+                            break
+                        elif self.comp_board.is_game_finished():
+                            self.end_game()
+                            break
+                    elif self.is_human_ships_added == False:
+                        if isinstance(col, int):
+                            if self.comp_board.add_ships_manual(self.number_of_human_ships, row, col) == True:
+                                print "Zwrocil True, zwiekszam dlugosc statku."
+                                self.number_of_human_ships += 1
+                                print "Aktualnie number_of_human_ships {} / {} number_of_ships".format(self.number_of_human_ships, self.number_of_ships)
+                            if self.number_of_human_ships == self.number_of_ships+1:
+                                self.is_human_ships_added = True
+            if self.is_human_ships_added:
                 self._refresh_view()
+            else:
+                self.start_game()
+            # if not self.is_human_ships_added:
+            #     self.start_game()
+            # if not self.board.is_game_finished() and not self.comp_board.is_game_finished():
+            #     self._refresh_view()
 
     def _refresh_view(self):
         self.screen.fill(gray)
@@ -48,52 +74,88 @@ class Game(object):
         self.draw_shots()
         pygame.display.update()
 
-    def debug(self):
-        print("Bord before adding ship on [0][0]: ")
-        for row in self.board.grid:
-            print(row)
-        self.board.add_ship(length=3, orientation=Board.HORIZONTAL, row=0, col=0)
-        print("Board after adding ship")
-        for row in self.board.grid:
-            print(row)
+    def start_game(self):
+        self.screen.fill(gray)
+        self.draw_empty_board()
+        pygame.display.update()
+
+    def computer_turn(self):
+        if self.comp_board.last_shot:
+            self.comp_board.near_shot()
+        else:
+            self.comp_board.random_shot()
+        self.player = Game.HUMAN
 
     def draw_shots(self):
-        self.shots = self.myfont.render("You hit "+(str(self.hit_shots)+" from "+str(self.total_shots))+" ships", 1, (255,255,255))
+        self.shots = self.myfont.render("You hit "+(str(self.board.hit_shots)+" from "+str(self.board.total_ships_length))+" piece of ship", 1, (255,255,255))
         self.screen.blit(self.shots, (10,10))
 
-    def _paint_tile(self, row, col, color):
-        left, top = self._left_top_coords_tile(row, col)
+    def _paint_tile(self, player, row, col, color):
+        # if player == "human":
+        left, top = self._left_top_coords_tile(player, row, col)
         pygame.draw.rect(self.screen, color, (left, top, tile_size, tile_size))
-
+        # elif player=="computer":
+        #     left = row * tile_size + margin_x + BOARDWIDTH*tile_size + 100
+        #     top = col * tile_size + margin_y
+        #     pygame.draw.rect(self.screen, color, (left, top, tile_size, tile_size))
     def draw(self):
         for row in xrange(self.board.height):
             for col in xrange(self.board.width):
                 color = self.board.get_color(row, col)
-                self._paint_tile(col, row, color)
+                self._paint_tile("human", col, row, color)
 
-        #Draw horizontal lines
+        for row in xrange(self.comp_board.height):
+            for col in xrange(self.comp_board.width):
+                color = self.comp_board.get_color(row, col)
+                self._paint_tile("computer", col, row, color)
+
+        #Drawing lines
+        board_width = BOARDWIDTH*tile_size
+        board_height = BOARDHEIGHT*tile_size
+        #Draw HUMAN horizontal lines
         for x in xrange(0, (BOARDWIDTH + 1) * tile_size, tile_size):
-            pygame.draw.line(self.screen, darkgrey, (margin_x, margin_y + x), (margin_x + tile_size * BOARDWIDTH, margin_y + x))
-        #     pygame.draw.line(self.screen, darkgrey, (x + margin_x + MARKERSIZE,
-        #         margin_y + MARKERSIZE), (x + margin_x + MARKERSIZE,
-        #         window_height - margin_y))
-        #Draw vertical lines
-        for y in xrange(0, (BOARDHEIGHT + 1) * tile_size, tile_size):
-            pygame.draw.line(self.screen, darkgrey, (margin_x + y, margin_y), (margin_x + y, margin_y + tile_size * BOARDHEIGHT))
-            # pygame.draw.line(self.screen, darkgrey, (margin_x,margin_y), (margin_x*tile_size*BOARDHEIGHT+y, margin_y))
+            pygame.draw.line(self.screen, darkgrey, (margin_x, margin_y + x), (margin_x + board_width, margin_y + x))
 
-            # pygame.draw.line(self.screen, darkgrey, (margin_x + MARKERSIZE, y +
-            #     margin_y + MARKERSIZE), (window_width - (display_width + MARKERSIZE *
-            #     2), y + margin_y + MARKERSIZE))
+        #Draw COMPUTER horizontal lines
+        for x in xrange(0, (BOARDWIDTH + 1) * tile_size, tile_size):
+            pygame.draw.line(self.screen, darkgrey, (margin_x + board_width + 100, margin_y + x), (margin_x + 2 * board_width + 100, margin_y + x))
+
+        #Draw HUMAN vertical lines
+        for y in xrange(0, (BOARDHEIGHT + 1) * tile_size, tile_size):
+            pygame.draw.line(self.screen, darkgrey, (margin_x + y, margin_y), (margin_x + y, margin_y + board_height))
+
+        #Draw COMPUTER vertical lines
+        for y in xrange(0, (BOARDHEIGHT + 1) * tile_size, tile_size):
+            pygame.draw.line(self.screen, darkgrey, (margin_x + y + board_height + 100, margin_y), (margin_x + y + board_height + 100, margin_y + board_height))
+
+    def draw_empty_board(self):
+        for row in xrange(self.comp_board.height):
+            for col in xrange(self.comp_board.width):
+                color = self.comp_board.get_color(row, col)
+                self._paint_tile("computer", col, row, color)
+        #Drawing lines
+        board_width = BOARDWIDTH*tile_size
+        board_height = BOARDHEIGHT*tile_size
+        #Draw COMPUTER horizontal lines
+        for x in xrange(0, (BOARDWIDTH + 1) * tile_size, tile_size):
+            pygame.draw.line(self.screen, darkgrey, (margin_x + board_width + 100, margin_y + x), (margin_x + 2 * board_width + 100, margin_y + x))
+
+        #Draw COMPUTER vertical lines
+        for y in xrange(0, (BOARDHEIGHT + 1) * tile_size, tile_size):
+            pygame.draw.line(self.screen, darkgrey, (margin_x + y + board_height + 100, margin_y), (margin_x + y + board_height + 100, margin_y + board_height))
+
 
     def end_game(self):
         self.screen.fill(gray)
-        self.label = self.myfont.render("You won!", 1, (255,255,255))
+        self.label = self.myfont.render("Game end!", 1, (255,255,255))
+        if self.board.is_game_finished():
+            self.label = self.myfont.render("You won!", 1, (255,255,255))
+        elif self.comp_board.is_game_finished():
+            self.label = self.myfont.render("Computer won!", 1, (255,255,255))
         self.screen.blit(self.label, ((window_width/2)-80, window_height/2))
         pygame.display.update()
-        print("Koniec gry")
 
-    def _get_tile_at_pixel(self, x, y):
+    def _get_tile_at_pixel(self, is_human_ships_added, x, y):
         """
         Function finds the corresponding tile coordinates of pixel at top left, defaults to (None, None) given a coordinate.
 
@@ -101,15 +163,26 @@ class Game(object):
         y -> int; y position of pixel
         returns tuple (tilex, tiley)
         """
-        for tilex in xrange(BOARDWIDTH):
-            for tiley in xrange(BOARDHEIGHT):
-                left, top = self._left_top_coords_tile(tilex, tiley)
-                tile_rect = pygame.Rect(left, top, TILESIZE, TILESIZE)
-                if tile_rect.collidepoint(x, y):
-                    return (tilex, tiley)
-        return (None, None)
+        if is_human_ships_added == True:
+            player = "human"
+            for tilex in xrange(BOARDWIDTH):
+                for tiley in xrange(BOARDHEIGHT):
+                    left, top = self._left_top_coords_tile(player, tilex, tiley)
+                    tile_rect = pygame.Rect(left, top, TILESIZE, TILESIZE)
+                    if tile_rect.collidepoint(x, y):
+                        return (tilex, tiley)
+            return (None, None)
+        else:
+            player = "computer"
+            for tilex in xrange(BOARDWIDTH):
+                for tiley in xrange(BOARDHEIGHT):
+                    left, top = self._left_top_coords_tile(player, tilex, tiley)
+                    tile_rect = pygame.Rect(left, top, TILESIZE, TILESIZE)
+                    if tile_rect.collidepoint(x, y):
+                        return (tilex, tiley)
+            return (None, None)
 
-    def _left_top_coords_tile(self, row, col):
+    def _left_top_coords_tile(self, player, row, col):
         """
         Function calculates and returns the pixel of the tile in the top left corner
 
@@ -117,8 +190,11 @@ class Game(object):
         col -> int; y position of tile
         returns tuple (int, int) which indicates top-left pixel coordinates of tile
         """
-        left = row * tile_size + margin_x
-        # left = row * tile_size + margin_x + MARKERSIZE
-        top = col * tile_size + margin_y
-        # top = tiley * tile_size + margin_y + MARKERSIZE
-        return (left, top)
+        if player == "human":
+            left = row * tile_size + margin_x
+            top = col * tile_size + margin_y
+            return (left, top)
+        elif player == "computer":
+            left = row * tile_size + margin_x + BOARDWIDTH*tile_size + 100
+            top = col * tile_size + margin_y
+            return (left, top)
